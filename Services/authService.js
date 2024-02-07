7e9d16e8b38d09f6ef1fd64f207cc4fec28ff1d0
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-02-08 00:11:53 +0200

    Update authService.js

--- Services/authService.js
+++ Services/authService.js
@@ -4,18 +4,24 @@ const jwt = require('../lib/jsonwebtoken');
 
 const User = require('../models/User');
 
+const SECRET = 'fghfdg7dfy7gbyfd87gf7sgsudajadkjaadhfdk'
 
-exports.register = (userData) => {
+
+exports.register = async (userData) => {
     if (userData.password !== userData.rePassword) {
         throw new Error('Password missmatch');
     }
-    const user = User.findOne({ email: userData.email });
+    const user = await User.findOne({ email: userData.email });
 
     if (user) {
         throw new Error('User already exists');
     }
 
-    return User.create(userData);
+    const createdUser = await User.create(userData);
+
+    const token = await generateToken(createdUser);
+
+    return token;
 };
 
 exports.login = async (email, password) => {
@@ -30,5 +36,18 @@ exports.login = async (email, password) => {
     if (!isValid) {
         throw new Error('Email or password is invalid');
     }
+
+    const token = await generateToken(user)
+    return token;
+
+}
+
+function generateToken(user) {
+    const payload = {
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+    }
+    return jwt.sign(payload, SECRET, { expiresIn: '2h' });
 
 }
