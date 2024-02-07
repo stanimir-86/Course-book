ad6351f1fc3b80a74524933a2f1232e2df0fbe50
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-02-08 00:41:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const handlebars = require('express-handlebars');
 const mongoose = require('mongoose');
 
 const routes = require('./routes');
+const { authMidleware } = require('./middlewares/authMidleware');
 
 const app = express();
 
 app.use(express.static('public'));//искам за всичките пътища да преминат през public
 app.use(express.urlencoded({ extended: false }));//с това от req.body си вземам form data
+app.use(authMidleware)
 
 app.engine('hbs', handlebars.engine({ extname: 'hbs', }));
 app.set('view engine', 'hbs');
