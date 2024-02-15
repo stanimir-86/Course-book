a0c4bb45915b8a290543d9046d9c5e3c4b72d60e
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-02-15 21:24:18 +0200

    Update Course.js

--- models/Course.js
+++ models/Course.js
@@ -3,37 +3,52 @@ const mongoose = require('mongoose');
 const courseSchema = new mongoose.Schema({
     title: {
         type: String,
+        minlength: 5,
         required: true,
     },
     type: {
         type: String,
+        minlength: 3,
         required: true,
     },
     certificate: {
         type: String,
+        minlength: 2,
         required: true,
     },
     image: {
         type: String,
+        match: /^https?:\/\//,
         required: true,
     },
     description: {
         type: String,
+        minlength: 10,
         required: true,
     },
     price: {
         type: Number,
+        min: 0,
         required: true,
     },
     signUpList: [{
         type: mongoose.Types.ObjectId,
         ref: 'User'
     }],
+
+    createdAt: Date,
+
     owner: {
         type: mongoose.Types.ObjectId,
         ref: 'User'
     },
 
+});
+
+courseSchema.pre('save', function () {
+    if (!this.createdAt) {
+        this.createdAt = Date.now();
+    }
 });
 
 const Course = mongoose.model('Course', courseSchema);
