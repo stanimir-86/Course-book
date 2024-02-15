0df4251dc00b57b409929d43d3fd0a407356d1bf
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-02-15 10:55:13 +0200

    Add 404 page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -4,9 +4,6 @@ const { isAuth } = require('../middlewares/authMidleware.js');
 router.get('/', (req, res) => {
     res.render('home');
 });
-//test delete this
-router.get('/authorize-test', isAuth, (req, res) => {
-    res.send('You are auth');
-})
+
 
 module.exports = router;
