c66374ab0863b41c7bb8146f76ea27c1e0904ab6
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-02-15 22:16:52 +0200

    Update homeController.js

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,11 +1,18 @@
 const router = require('express').Router();
 const courseService = require('../Services/courseService.js');
-
+const userService = require('../Services/userService.js');
+const { isAuth } = require('../middlewares/authMidleware.js')
 router.get('/', async (req, res) => {
 
     const latestCourses = await courseService.getLatest().lean();
     res.render('home', { latestCourses });
 });
+router.get('/profile', isAuth, async (req, res) => {
+    const user = await userService.getInfo(req.user._id).lean();
+    const createdCoursesCount = user.createdCourses.length;
+    const signeUpCoursesCount = user.signeUpCourses.length;
 
+    res.render('profile', { user, createdCoursesCount, signeUpCoursesCount });
+})
 
 module.exports = router;
