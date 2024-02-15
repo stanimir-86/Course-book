4f11fc080dbdda92f246a5e98d18bb355ca89e1e
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-02-15 21:24:20 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,27 +4,29 @@ const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
+        minLength: 2,
         required: [true, "Username is required"],
     },
     email: {
         type: String,
+        minLength: 10,
         required: [true, "Email is required"],
-
         unique: true,
 
     },
     password: {
         type: String,
+        minlength: 4,
         required: [true, "Password is required"],
 
     },
     createdCourses: [{
-        type:mongoose.Types.ObjectId,
-        ref:'Course',
+        type: mongoose.Types.ObjectId,
+        ref: 'Course',
     }],
     signeUpCourses: [{
-        type:mongoose.Types.ObjectId,
-        ref:'Course',
+        type: mongoose.Types.ObjectId,
+        ref: 'Course',
     }],
 
 });
